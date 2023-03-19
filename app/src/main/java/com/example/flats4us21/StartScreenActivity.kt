6af5adc06ff232f90dd19c44ce8b1e2129b4b04d
Jakub Opasiński <s22677@pjwstk.edu.pl>
2023-03-19 01:55:18 +0100

    Minor changes

--- app/src/main/java/com/example/flats4us21/StartScreenActivity.kt
+++ app/src/main/java/com/example/flats4us21/StartScreenActivity.kt
@@ -5,21 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.example.flats4us21.databinding.ActivityStartScreenBinding
 
 class StartScreenActivity : AppCompatActivity() {
-
+    private lateinit var binding : ActivityStartScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_screen)
+        binding = ActivityStartScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
-        buttonLogin.setOnClickListener {
+
+        binding.buttonLogin.setOnClickListener {
             Toast.makeText(this, "Login button clicked", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
         val buttonRegister = findViewById<Button>(R.id.buttonRegister)
-        buttonLogin.setOnClickListener {
+        binding.buttonRegister.setOnClickListener {
             Toast.makeText(this, "Register button clicked", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
