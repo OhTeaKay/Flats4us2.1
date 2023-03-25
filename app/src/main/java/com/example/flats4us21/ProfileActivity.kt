caf4eabdfb1e62992c092add36855cd01359e92f
okozluk <oskarmail@onet.pl>
2023-03-25 14:47:43 +0100

    profile activity

---
+++ app/src/main/java/com/example/flats4us21/ProfileActivity.kt
@@ -0,0 +1,21 @@
+package com.example.flats4us21
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.InputType
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+
+
+class ProfileActivity: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+
+    }
+}
