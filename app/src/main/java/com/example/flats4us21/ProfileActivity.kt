5dbbe62ad980f340f6fa7cf2beccf529a2d9cc5f
okozluk <oskarmail@onet.pl>
2023-03-27 14:20:50 +0200

    added menu to profile

--- app/src/main/java/com/example/flats4us21/ProfileActivity.kt
+++ app/src/main/java/com/example/flats4us21/ProfileActivity.kt
@@ -15,7 +15,19 @@ class ProfileActivity: AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.about -> Toast.makeText(this,"About Selected", Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this,"Settings Selected",Toast.LENGTH_SHORT).show()
+            R.id.exit -> Toast.makeText(this,"Exit Selected",Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
