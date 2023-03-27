a7fa83b4ce40ae7119e68ae7d06188e89c0748be
Jakub Opasiński <s22677@pjwstk.edu.pl>
2023-03-27 17:37:32 +0200

    fix2

---
+++ app/src/main/java/com/example/flats4us21/services/IService.kt
@@ -0,0 +1,10 @@
+package com.example.flats4us21.services
+
+import com.example.flats4us21.data.Offer
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface IService {
+
+}
